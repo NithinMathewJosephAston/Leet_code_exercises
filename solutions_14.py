7d6d47eba24a1b32a4e463f31a08b7209317607e
NithinMathewJosephAston <102992254+NithinMathewJosephAston@users.noreply.github.com>
2023-03-17 03:37:32 +0000

    Add files via upload
    Finding the square root without using one of the modules available.

---
+++ solutions_14.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def mySqrt(self, x: int) -> int:
+        low = 0
+        high = x // 2 + 1
+        while low < high:
+            mid = low + (high - low + 1) // 2
+            if mid * mid > x:
+                high = mid - 1
+            else:
+                low = mid
+
+        return low
+
+gems = Solution()
+print(gems.mySqrt(8))
